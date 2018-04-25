c4f6d84a83b9545fc4cf4d63d6ba8875c33c2bd2
Sighmir <guistoppa1995@gmail.com>
2018-04-24 21:35:01 -0300

    chore(config.characters): add character config example

--- packages/YARP/configs/characters.js
+++ packages/YARP/configs/characters.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let config = {
+  'Sighmir Greenroom': {
+    groups: ['Cop'],
+    enter: (player) => {player.notify('~y~You should eat something.');}
+  }
+}
+
+module.exports = config;
